ac1a6c07969acdbfb555d590a0f9ca72622d764e
xiaoran27 <xiaoran27@163.com>
2022-10-16 22:08:26 +0800

    增加log函数

---
+++ extension/custom/common/ext/model/log.php
@@ -0,0 +1,26 @@
+<?php
+
+    /**
+     * Save a log.
+     *
+     * @param  string $message
+     * @param  string $file
+     * @param  string $line
+     * @access public
+     * @return void
+     */
+    public function log($message, $file = '', $line = '')
+    {
+        global $app;
+
+        $log = "\n" . date('H:i:s') . " $message";
+        if($file) $log .= " f[$file]";
+        if($line) $log .= " L=$line";
+       
+
+        $file = $app->getLogRoot() . 'banniu.' . date('Ymd') . '.log.php';
+        if(!is_file($file)) file_put_contents($file, "<?php\n die();\n?>\n");
+
+        $fh = @fopen($file, 'a');
+        if($fh) fwrite($fh, $log) && fclose($fh);
+    }
