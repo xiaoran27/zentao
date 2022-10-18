7466f0c76777ded455f53f58233e7a94c67a48b6
xiaoran27 <xiaoran27@163.com>
2022-10-18 18:26:28 +0800

    统计报表-bug{客户名称}

--- extension/custom/bug/ext/lang/zh-cn/lang-ext-bug.php
+++ extension/custom/bug/ext/lang/zh-cn/lang-ext-bug.php
@@ -1,3 +1,11 @@
 <?php
 
 $lang->bug->purchaser      = '客户名称';
+
+
+/* 统计报表。*/
+$lang->bug->report->charts['purchaser']         = "按{$lang->bug->purchaser}来进行统计";
+$lang->bug->report->purchaser       = new stdclass();
+$lang->bug->report->purchaser->item       = $lang->bug->purchaser;
+$lang->bug->report->purchaser->graph       = new stdclass();
+$lang->bug->report->purchaser->graph->xAxisName      = $lang->bug->purchaser;
