b0477e281bb9033a7d0d6ae0cf4f7b118f05d200
xiaoran27 <xiaoran27@163.com>
2022-11-02 14:29:11 +0800

    zt_purchaser插入"正马软件"

--- extension/custom/banniu.sql
+++ extension/custom/banniu.sql
@@ -1,23 +1,25 @@
 
--- 2022-10-16 22:00:45
-ALTER TABLE zentao.zt_story ADD bzCategory varchar(16)  DEFAULT 'LKA' NULL COMMENT '客户类型';
-ALTER TABLE zentao.zt_story ADD prCategory varchar(16)  DEFAULT 'product' NULL COMMENT '需求分类';
-ALTER TABLE zentao.zt_story ADD uatDate DATE NULL COMMENT 'UAT日期';
+-- use zentao;
 
-ALTER TABLE zentao.zt_story ADD purchaser varchar(64) NULL COMMENT '客户名称';
+-- 2022-10-16 22:00:45
+ALTER TABLE zt_story ADD bzCategory varchar(16)  DEFAULT 'LKA' NULL COMMENT '客户类型';
+ALTER TABLE zt_story ADD prCategory varchar(16)  DEFAULT 'product' NULL COMMENT '需求分类';
+ALTER TABLE zt_story ADD uatDate DATE NULL COMMENT 'UAT日期';
+
+ALTER TABLE zt_story ADD purchaser varchar(64) NULL COMMENT '客户名称';
 
 
 -- 2022-10-16 22:11:41
-ALTER TABLE zentao.zt_bug ADD purchaser varchar(256) NULL COMMENT '客户名称'; 
+ALTER TABLE zt_bug ADD purchaser varchar(256) NULL COMMENT '客户名称'; 
 
 
 -- 2022-10-20 20:30:15
-ALTER TABLE zentao.zt_bug ADD occursEnv varchar(128)  DEFAULT 'online' NULL COMMENT '发生环境'; 
-ALTER TABLE zentao.zt_bug ADD feedbackTime datetime   NULL COMMENT '反馈时间'; 
-ALTER TABLE zentao.zt_bug ADD collectTime datetime   NULL COMMENT '收集时间'; 
+ALTER TABLE zt_bug ADD occursEnv varchar(128)  DEFAULT 'online' NULL COMMENT '发生环境'; 
+ALTER TABLE zt_bug ADD feedbackTime datetime   NULL COMMENT '反馈时间'; 
+ALTER TABLE zt_bug ADD collectTime datetime   NULL COMMENT '收集时间'; 
 
 -- 2022-10-27 21:09:50
-ALTER TABLE zentao.zt_story ADD responseResult smallint  DEFAULT 0 NULL COMMENT '响应结果'; 
+ALTER TABLE zt_story ADD responseResult smallint  DEFAULT 0 NULL COMMENT '响应结果'; 
 
 
 -- 2022-10-31 10:19:14
@@ -35,3 +37,5 @@ CREATE TABLE `zt_purchaser` (
   PRIMARY KEY (`ID`),
   UNIQUE KEY `zt_purchaser_code_IDX` (`code`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='客户信息';
+INSERT INTO zt_purchaser( code, name, category)
+VALUES('hzzmrjkjyxgs', '杭州正马软件科技有限公司', 'LKA'),( 'shzmrjkjyxgs', '上海正马软件科技有限公司', 'LKA');
