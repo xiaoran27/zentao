f1cf530ac8855b03a6d9d9df11cb860002d75e0d
xiaoran27 <xiaoran27@163.com>
2022-11-03 15:55:21 +0800

    一定要倒序写回滚的分段SQL

--- extension/custom/banniu_uninstall.sql
+++ extension/custom/banniu_uninstall.sql
@@ -5,10 +5,30 @@
 
 -- 每次需要更新SQL段
 -- sql.start.banniu_rel{yymmdd}
---  SQL语句
+--  时间倒序的SQL语句
 -- sql.end.banniu_rel{yymmdd}
 
+
+
 -- sql.start.banniu_rel221102
+
+-- 2022-10-31 10:19:14
+DROP TABLE IF EXISTS  `zt_purchaser` ;
+
+-- 2022-10-27 21:09:50
+ALTER TABLE zt_story DROP COLUMN  IF EXISTS  responseResult ; 
+
+
+-- 2022-10-20 20:30:15
+ALTER TABLE zt_bug DROP COLUMN  IF EXISTS  occursEnv ; 
+ALTER TABLE zt_bug DROP COLUMN  IF EXISTS  feedbackTime ; 
+ALTER TABLE zt_bug DROP COLUMN  IF EXISTS  collectTime ; 
+
+
+-- 2022-10-16 22:11:41
+ALTER TABLE zt_bug DROP COLUMN  IF EXISTS  purchaser ; 
+
+
 -- 2022-10-16 22:00:45
 ALTER TABLE zt_story DROP COLUMN  IF EXISTS  bzCategory ;
 ALTER TABLE zt_story DROP COLUMN  IF EXISTS  prCategory ;
@@ -17,20 +37,5 @@ ALTER TABLE zt_story DROP COLUMN  IF EXISTS  uatDate ;
 ALTER TABLE zt_story DROP COLUMN  IF EXISTS  purchaser ;
 
 
--- 2022-10-16 22:11:41
-ALTER TABLE zt_bug DROP COLUMN  IF EXISTS  purchaser ; 
-
-
--- 2022-10-20 20:30:15
-ALTER TABLE zt_bug DROP COLUMN  IF EXISTS  occursEnv ; 
-ALTER TABLE zt_bug DROP COLUMN  IF EXISTS  feedbackTime ; 
-ALTER TABLE zt_bug DROP COLUMN  IF EXISTS  collectTime ; 
-
--- 2022-10-27 21:09:50
-ALTER TABLE zt_story DROP COLUMN  IF EXISTS  responseResult ; 
-
-
--- 2022-10-31 10:19:14
-DROP TABLE IF EXISTS  `zt_purchaser` ;
 
 -- sql.end.banniu_rel221102
