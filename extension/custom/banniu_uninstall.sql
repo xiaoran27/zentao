1402692bb8c3e24404cd8497c872f34742ec35b7
xiaoran27 <xiaoran27@163.com>
2022-12-06 23:00:27 +0800

    banniu_rel221207 SQL卸载

--- extension/custom/banniu_uninstall.sql
+++ extension/custom/banniu_uninstall.sql
@@ -9,6 +9,52 @@
 -- sql.end.banniu_rel{yymmdd}
 
 
+
+-- sql.start.banniu_rel221207
+
+-- 2022-11-28 20:31:58
+
+ALTER TABLE zt_purchaser MODIFY COLUMN  mtime datetime  NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ;
+
+ALTER TABLE zt_story ADD prLevel varchar(32) DEFAULT 'C' NOT NULL COMMENT '需求等级';
+
+ALTER TABLE zt_story MODIFY COLUMN responseResult varchar(32) DEFAULT '0' NOT NULL COMMENT '响应结果';
+update zt_story set responseResult='0' where responseResult = 'todo';
+update zt_story set responseResult='1' where responseResult = 'reject';
+update zt_story set responseResult='2' where responseResult = 'research';
+update zt_story set responseResult='3' where responseResult = 'accept';
+ALTER TABLE zt_story DROP COLUMN   IF EXISTS rspRecievedTime ;
+ALTER TABLE zt_story DROP COLUMN   IF EXISTS rspRejectTime ;
+ALTER TABLE zt_story DROP COLUMN   IF EXISTS rspResearchTime ;
+ALTER TABLE zt_story DROP COLUMN   IF EXISTS rspAcceptTime ;
+
+DROP TRIGGER IF EXISTS zentao.tri_story_bu;
+
+ALTER TABLE zt_story DROP COLUMN   IF EXISTS  bizProject ;
+
+ALTER TABLE zt_storyspec DROP COLUMN   IF EXISTS bizStage  ;
+ALTER TABLE zt_storyspec DROP COLUMN   IF EXISTS bizTarget ;
+ALTER TABLE zt_storyspec DROP COLUMN   IF EXISTS bizValue ;
+
+-- sql.end.banniu_rel221207
+
+-- sql.start.banniu_rel221125
+
+-- 2022-11-18 17:27:01
+delete from zt_purchaser where mtime = '2022-11-25';
+
+-- sql.end.banniu_rel221125
+
+
+
+-- sql.start.banniu_rel221125
+
+-- 2022-11-18 17:27:01
+delete from zt_purchaser where mtime = '2022-11-25';
+
+-- sql.end.banniu_rel221125
+
+
 -- sql.start.banniu_rel221107
 
 --2022-11-8 16:34:01
