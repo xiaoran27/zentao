a41901c9e60ae078831fc9ee5317e0d73fa15efa
xique-0wfpn <xique@bytenew.com>
2023-04-20 21:55:48 +0800

    压缩custom的脚本

---
+++ extension/7z-custom.bat
@@ -0,0 +1,22 @@
+﻿@echo off
+
+:: 压缩custom的二开包
+
+
+pushd  %cd%
+
+cd /d %~dp0
+
+Set yyyymmdd=%Date:~0,4%%Date:~5,2%%Date:~8,2%
+Set datetime=%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%
+
+set zipfile=zentao_extension_custom_%yyyymmdd%.zip
+if exist %zipfile% del %zipfile%
+
+7z a -xr!custom\banniu_rel*.sql -xr!custom\zentao*bk.sql -xr!custom\banniu_uninstall_rel*.sql -xr!custom\客户名称转拼音*.* -xr!custom\已成交班牛企业版老客户*.* -xr!custom\星链客户*.* -xr!custom\json2csv.py -xr!custom\demo %zipfile% custom
+
+set sqlfile=banniu_rel%yyyymmdd:~2,6%.sql
+if exist %sqlfile% del %sqlfile%
+copy /Y custom\%sqlfile% .
+
+popd
