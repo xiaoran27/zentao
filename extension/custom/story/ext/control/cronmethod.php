5db909f726b90ce9663fd04802dbf60a698d510e
xique-0wfpn <xique@bytenew.com>
2023-05-05 09:46:34 +0800

    updateStoryAcceptTimeByTask支持cron

--- extension/custom/story/ext/control/cronmethod.php
+++ extension/custom/story/ext/control/cronmethod.php
@@ -4,6 +4,25 @@ include '../../../../../module/story/control.php';
 
 class myStory extends story
 {
+
+    /**
+     * 据task+action找到开发的最早启动时间作为对应产品需求的prd完成时间(rspAcceptTime)
+     *
+     * @param  int    $days=1  一天内
+     * @param  int    $taskID=0 表示全部
+     * @param  bool   $createAction
+     * @access public
+     * @return void
+     */
+    public function updateStoryAcceptTimeByTask($days=1, $taskID=0, $createAction = true)
+    {
+ 
+        $data = $this->story->updateStoryRspAcceptTimeByTask($days, $taskID, $createAction );
+        echo json_encode((array('result' => 'success', 'data' => $data, 'days' => $days, 'taskID' => $taskID, 'createAction' => $createAction)) ,JSON_UNESCAPED_UNICODE );
+        // return $this->send((array('result' => 'success', 'data' => $data, 'days' => $days, 'taskID' => $taskID, 'createAction' => $createAction));
+        
+    }
+
     /**
      * 更新业务需求的status和stage
      *
