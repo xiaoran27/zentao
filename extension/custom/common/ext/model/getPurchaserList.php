537a6d04ca79a44051b3d41f7d160a4d64761eef
xique-0wfpn <xique@bytenew.com>
2023-05-24 21:57:29 +0800

    去除过多的客户名称日志

--- extension/custom/common/ext/model/getPurchaserList.php
+++ extension/custom/common/ext/model/getPurchaserList.php
@@ -8,7 +8,7 @@ public function getPurchaserList($codeOrName='')
     $datas = $this->dao->select('*')->from($TABLE_PURCHASER)->where($where)->fetchAll();
     if(!$datas) return $purchaserList;
     foreach($datas as $key => $data) {
-        $this->loadModel('common')->log(json_encode($data,JSON_UNESCAPED_UNICODE), __FILE__, __LINE__);
+        // $this->loadModel('common')->log(json_encode($data,JSON_UNESCAPED_UNICODE), __FILE__, __LINE__);
         if (empty($codeOrName)) {
             $value = $data->name ;
             $purchaserList[$data->code] =  $value ; 
