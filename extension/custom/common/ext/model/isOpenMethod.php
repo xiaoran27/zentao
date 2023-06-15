2ee3b9c3142f8189c1d42d441c909f086d1b92c1
xique-0wfpn <xique@bytenew.com>
2023-06-15 20:19:35 +0800

    原始文件

---
+++ extension/custom/common/ext/model/isOpenMethod.php
@@ -0,0 +1,32 @@
+<?php
+
+
+    /**
+     * Juage a method of one module is open or not?
+     *
+     * @param  string $module
+     * @param  string $method
+     * @access public
+     * @return bool
+     */
+    public function isOpenMethod($module, $method)
+    {
+        if(in_array("$module.$method", $this->config->openMethods)) return true;
+
+        if($module == 'block' and $method == 'main' and isset($_GET['hash'])) return true;
+
+        if($this->loadModel('user')->isLogon() or ($this->app->company->guest and $this->app->user->account == 'guest'))
+        {
+            if(stripos($method, 'ajax') !== false) return true;
+            if($module == 'block') return true;
+            if($module == 'my' and $method == 'guidechangetheme') return true;
+            if($module == 'misc' and $method == 'downloadclient') return true;
+            if($module == 'misc' and $method == 'changelog')  return true;
+            if($module == 'tutorial' and $method == 'start')  return true;
+            if($module == 'tutorial' and $method == 'index')  return true;
+            if($module == 'tutorial' and $method == 'quit')   return true;
+            if($module == 'tutorial' and $method == 'wizard') return true;
+            if($module == 'product' and $method == 'showerrornone') return true;
+        }
+        return false;
+    }
---
+++ extension/custom/user/ext/config/config-ext-user.php
@@ -0,0 +1,3 @@
+<?php
+
+global $lang, $app, $config;
